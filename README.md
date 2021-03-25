90a47357a44ccb74923619c8a466158561fbab94
Kaleb Bernou <kaleb.bernou444@gmail.com>
2021-03-25 01:20:48 -0400

    It does what I need for now

---
+++ README.md
@@ -0,0 +1,6 @@
+# Dummy Server
+It accepts requests. It logs them to the console. It does it all.
+
+## Requirements
+* Node
+* Nodemon is also nice
---
+++ index.js
@@ -0,0 +1,38 @@
+"use strict"
+const express = require("express"),
+      app = express(),
+      port = process.env.PORT || 3535;
+
+app.route("/200/*")
+    .get((req, res) => {
+        console.log(`200 GET to ${req.url}`)
+        res.send("OK").status(200)
+    })
+    .post((req, res) => {
+        console.log(`200 POST to ${req.url}`)
+        res.send("OK").status(200)
+    });
+
+app.route("/400/*")
+    .get((req, res) => {
+        console.log(`400 GET to ${req.url}`)
+        res.send("400 You made a mistake").status(400)
+    })
+    .post((req, res) => {
+        console.log(`400 POST to ${req.url}`)
+        res.send("400 You made a mistake").status(400)
+    });
+
+app.route("/500/*")
+    .get((req, res) => {
+        console.log(`500 GET to ${req.url}`)
+        res.send("500 I made a mistake").status(500)
+    })
+    .post((req, res) => {
+        console.log(`500 POST to ${req.url}`)
+        res.send("500 I made a mistake").status(500)
+    });
+
+app.listen(port, () => {
+    console.info(`Started on ${port}`);
+})
